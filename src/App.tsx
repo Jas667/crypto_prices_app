d38209d32aa2acf5ee3fd51679c2caebfe110ef9
Jas667 <100931408+Jas667@users.noreply.github.com>
2023-08-03 10:14:49 +0100

    dropdown menu created

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,37 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios"; //instead of using fetch
-import CryptoSummary from "./components/CryptoSummary";
 import { Crypto } from "./Types";
+import CryptoSummary from "./components/CryptoSummary";
 
 function App() {
-  const [cryptos, setCryptos] = useState<Crypto[] | null>();
+  const [cryptos, setCryptos] = useState<Crypto[] | null>(null);
+  const [selected, setSelected] = useState<Crypto | null>();
   useEffect(() => {
     const url =
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en";
     axios.get(url).then((response) => {
-      console.log(response.data);
       setCryptos(response.data);
     });
   }, []);
   return (
     <>
       <div>
-        {cryptos
-          ? cryptos.map((crypto) => {
-              return <CryptoSummary crypto={crypto} />;
-            })
-          : null}
+        <select onChange={(e) => {
+          const c = cryptos?.find((x) => x.id === e.target.value)
+          setSelected(c)
+        }}
+        defaultValue="default"
+        >
+          <option value="default">Choose an option</option>
+          {cryptos
+            ? cryptos.map((crypto) => {
+                return <option key={crypto.id} value={crypto.id}>{crypto.name}</option>;
+              })
+            : null}
+        </select>
       </div>
+      {selected ? <CryptoSummary crypto={selected} /> : null}
     </>
   );
 }
